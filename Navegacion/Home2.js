dfed954f577038d024f0a13f12730a3a5d321b05
WaCnerDev <vvacnerdev@gmail.com>
2024-06-05 14:07:04 -0600

    initial commit: develop the navigation component

---
+++ Navegacion/Home2.js
@@ -0,0 +1,14 @@
+import React from "react"
+import {View, Text, StyleSheet, TouchableOpacity} from "react-native";
+
+const Home2 = () =>{
+    return(
+   <View>
+        <Text>Home2</Text>
+    </View>
+  );
+   
+
+}
+
+export default Home2;
---
+++ Navegacion/HomeBtn.js
@@ -0,0 +1,12 @@
+import React from "react"
+import {View, Text, StyleSheet, TouchableOpacity} from "react-native";
+
+const HomeBtn = () =>{
+    return(
+   <View>
+        <Text>HomeBtn</Text>
+    </View>
+  );
+}
+
+export default HomeBtn;
